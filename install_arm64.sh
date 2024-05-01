2317d9f138ee2a437416ea32b7a70f122b0c25ed
22226578zxc <128694993+22226578zxc@users.noreply.github.com>
2024-05-01 10:02:20 +0800

    Create install_arm64.sh

---
+++ install_arm64.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://assets.checkra.in/downloads/linux/cli/arm64/43019a573ab1c866fe88edb1f2dd5bb38b0caf135533ee0d6e3ed720256b89d0/checkra1n -q -O checkra1n
+mv ./checkra1n /usr/local/bin/checkra1n
+chmod +x /usr/local/bin/checkra1n
