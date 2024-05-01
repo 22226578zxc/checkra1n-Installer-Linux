60ad4bb36e9e974a679c60a962c4ca2f74331d53
22226578zxc <128694993+22226578zxc@users.noreply.github.com>
2024-05-01 10:16:28 +0800

    Create palera1n_arm64.sh

---
+++ palera1n_arm64.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.9.1/palera1n-linux-arm64 -q -O palera1n
+chmod +x ./palera1n
+mv ./palera1n /usr/local/bin/palera1n
